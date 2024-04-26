ba1c7ade189af265550b8777952cf9ea189e8bef
oscar33213 <133204943+oscar33213@users.noreply.github.com>
2024-04-26 21:13:07 +0200

    Add files via upload

---
+++ Proyectos/src/proyectos_simples/GeneradorMatriculas2.java
@@ -0,0 +1,31 @@
+package proyectos_simples;
+
+import java.util.*;
+
+public class GeneradorMatriculas2 {
+
+	public static void main(String[] args) {
+		
+		String numero = "";
+		String letra = "";
+		
+		
+		Random matricularandom = new Random();
+		
+		for(int i = 0; i<4; i++) {
+			numero+= matricularandom.nextInt(10);
+			
+		}
+		
+		for(int i = 0; i< 3; i++) {
+			letra += (char)(matricularandom.nextInt(26) + 'A');
+		}
+		
+		String matricula = numero + letra;
+		
+		System.out.println("La matricula es: " + matricula);
+		
+		
+	}
+
+}
